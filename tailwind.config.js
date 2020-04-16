24f85687a70ac47297f9ccf84665f1093a3a72b1
Nico Braun <nico.braun@concentrix.com>
2020-04-16 19:45:43 +0100

    implement 'add' dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,26 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
     extend: {
       width: {
-        "15px": "15px",
-        "14": "60px",
-        "72": "330px"
+        "sidebar": "60px",
+        "menu": "330px",
+        'dropdown-large': '400px',
+
       },
       colors: {
-        zengreen: "#78a300",
-        "teal-1000": "#03363D",
+        green: {
+          ...colors.green,
+          '500': "#78a300",
+        },
+        teal: {
+          ...colors.teal,
+          '900': "#03363D",
+        },
         gray: {
+          ...colors.gray,
           "100": "#F8F9F9",
-          "200": "#eeeeee",
-          "300": "#e0e0e0",
-          "400": "#bdbdbd",
-          "500": "#9e9e9e",
-          "600": "#757575",
-          "700": "#616161",
-          "800": "#424242",
-          "900": "#212121"
         }
       }
     }
@@ -27,7 +29,8 @@ module.exports = {
     borderWidth: ["responsive", "last"],
     borderRadius: ["responsive", "first", "last"],
     cursor: ["responsive", "focus"],
-    width: ["responsive", "focus"]
+    width: ["responsive", "focus"],
+    display: ['responsive', 'group-hover'],
   },
   plugins: []
 };
