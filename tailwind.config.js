58579ed54ab8d7b8b8397e4f37b6ffec686314b6
Nico Braun <nico.braun@concentrix.com>
2020-04-17 10:41:04 +0100

    make deployable without config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,5 @@
-const { colors } = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  theme: {
-    extend: {
-      width: {
-        "sidebar": "60px",
-        "menu": "330px",
-        'dropdown-large': '400px',
-
-      },
-      colors: {
-        green: {
-          ...colors.green,
-          '500': "#78a300",
-        },
-        teal: {
-          ...colors.teal,
-          '900': "#03363D",
-        },
-        gray: {
-          ...colors.gray,
-          "100": "#F8F9F9",
-        }
-      }
-    }
-  },
-  variants: {
-    cursor: ["responsive", "focus"],
-    width: ["responsive", "focus"],
-    display: ['responsive', 'group-hover'],
-  },
-  plugins: []
+  theme: {},
+  variants: {},
+  plugins: [],
 };
